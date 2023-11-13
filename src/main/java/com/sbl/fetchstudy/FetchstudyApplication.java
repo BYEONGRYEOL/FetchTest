bc53a83b35aa094047b0d4ee38c0c659a0f378dd
BYEONGRYEOL <lyulbyung@gmail.com>
2023-11-13 16:04:13 +0900

    수정x

---
+++ src/main/java/com/sbl/fetchstudy/FetchstudyApplication.java
@@ -0,0 +1,13 @@
+package com.sbl.fetchstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FetchstudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FetchstudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sbl/fetchstudy/FetchstudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbl.fetchstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FetchstudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
