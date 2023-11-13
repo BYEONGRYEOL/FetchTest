edb2883da11f50031ec37e0b50ee884923ba5d78
BYEONGRYEOL <lyulbyung@gmail.com>
2023-11-13 19:14:19 +0900

    Entity create 함수 추가

--- src/test/java/com/sbl/fetchstudy/factory/entity/MyEntityFactory.java
+++ src/test/java/com/sbl/fetchstudy/factory/entity/MyEntityFactory.java
@@ -2,17 +2,29 @@ package com.sbl.fetchstudy.factory.entity;
 
 import com.sbl.fetchstudy.entity.D_MTO_TW_FE;
 import com.sbl.fetchstudy.entity.O_MTO_TW_WFE;
+import com.sbl.fetchstudy.entity.cascade.D_MTO_TW_CP;
+import com.sbl.fetchstudy.entity.cascade.O_MTO_TW_CP;
 
 public class MyEntityFactory {
-    static int ownerMTOTwoWayFetchEagerNumber = 1;
+    static int ownerMTOTwoWayWithFetchEagerNumber = 1;
     static char dependentMTOTwoWayFetchEagerNumber = 'a';
     static int ownerMTOTwoWayFetchLazyNumber = 1;
     static char dependentMTOTwoWayFetchLazyNumber = 'a';
-    public static O_MTO_TW_WFE createOwnerMTOTwoWayFetchEager(){
-        return new O_MTO_TW_WFE("owner_" + ownerMTOTwoWayFetchEagerNumber++);
+    static int ownerMTOTwoWayWithFetchLazyWithCascadePersistNumber = 1;
+    static char dependentMTOTwoWayFetchLazyCascadePersistNumber = 'a';
+    public static O_MTO_TW_WFE createOwnerMTOTwoWayWithFetchEager(){
+        return new O_MTO_TW_WFE("owner_" + ownerMTOTwoWayWithFetchEagerNumber++);
     }
     public static D_MTO_TW_FE createDependentMTOTwoWayFetchEager(){
         return new D_MTO_TW_FE("dependent_" + dependentMTOTwoWayFetchEagerNumber++);
     }
     // TODO: 2023-11-13 FetchLazy create 함수
+    public static D_MTO_TW_CP createDependentMTOTwoWayFetchLazyCascadePersist(){
+        return new D_MTO_TW_CP("dependent_" + dependentMTOTwoWayFetchLazyCascadePersistNumber++);
+    }
+
+    public static O_MTO_TW_CP createOwnerMTOTwoWayWithFetchLazyWithCascadePersist(){
+        return new O_MTO_TW_CP("owner_" + ownerMTOTwoWayWithFetchLazyWithCascadePersistNumber++);
+    }
+
 }
